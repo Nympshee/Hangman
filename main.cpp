30b8e57d04869dc58f869c5e068f6b121738e745
Nympshee <125748788+Nympshee@users.noreply.github.com>
2024-08-09 23:59:20 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,155 +1,118 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <cstdlib>
 #include <ctime>
 #include <algorithm>
-#include <cctype> // Para verificar caracteres
-#include <stdlib.h>
+#include <cctype>
 
-#ifdef _WIN32
-#include <windows.h>
-#else
-#include <unistd.h>
-#endif
 
-void clearScreen() {
-#ifdef _WIN32
+using namespace std;
+
+const vector<string> HANGMAN = {
+    "  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
+    "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
+    "  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
+    "  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
+    "  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n=========",
+    "  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n=========",
+    "  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n========="
+};
+
+void limparTela() {
+    #ifdef _WIN32
     system("cls");
-#else
+    #else
     system("clear");
-#endif
+    #endif
 }
 
-void printWord(const std::vector<char>& guessed, const std::string& word) {
-    for (char c : word) {
-        if (std::find(guessed.begin(), guessed.end(), c) != guessed.end()) {
-            std::cout << c << ' ';
+string escolherPalavra() {
+    vector<string> palavras = {"abacaxi", "bicicleta", "chocolate", "dinossauro", "elefante"};
+    return palavras[rand() % palavras.size()];
+}
+
+void mostrarProgresso(const string& palavra, const vector<char>& letrasAdivinhadas) {
+    for (char letra : palavra) {
+        if (find(letrasAdivinhadas.begin(), letrasAdivinhadas.end(), letra) != letrasAdivinhadas.end()) {
+            cout << letra << " ";
         } else {
-            std::cout << "_ ";
+            cout << "_ ";
         }
     }
-    std::cout << std::endl;
+    cout << endl;
 }
 
-void printHangman(int chances) {
-    const char* hangman[] = {
-        "  +---+\n"
-        "  |   |\n"
-        "      |\n"
-        "      |\n"
-        "      |\n"
-        "      |\n"
-        "=========\n",
-        
-        "  +---+\n"
-        "  |   |\n"
-        "  O   |\n"
-        "      |\n"
-        "      |\n"
-        "      |\n"
-        "=========\n",
-
-        "  +---+\n"
-        "  |   |\n"
-        "  O   |\n"
-        "  |   |\n"
-        "      |\n"
-        "      |\n"
-        "=========\n",
-
-        "  +---+\n"
-        "  |   |\n"
-        "  O   |\n"
-        " /|   |\n"
-        "      |\n"
-        "      |\n"
-        "=========\n",
-
-        "  +---+\n"
-        "  |   |\n"
-        "  O   |\n"
-        " /|\\  |\n"
-        "      |\n"
-        "      |\n"
-        "=========\n",
-
-        "  +---+\n"
-        "  |   |\n"
-        "  O   |\n"
-        " /|\\  |\n"
-        " /    |\n"
-        "      |\n"
-        "=========\n",
-
-        "  +---+\n"
-        "  |   |\n"
-        "  O   |\n"
-        " /|\\  |\n"
-        " / \\  |\n"
-        "      |\n"
-        "=========\n"
-    };
-
-    std::cout << hangman[6 - chances];
+bool jogarNovamente() {
+    char resposta;
+    cout << "Quer jogar de novo? (s/n): ";
+    cin >> resposta;
+    return (resposta == 's' || resposta == 'S');
 }
 
 int main() {
-    std::vector<std::string> words = {"programacao", "computador", "terminal", "jogo"};
-    std::srand(std::time(nullptr));
-    std::string word = words[std::rand() % words.size()];
+    {
+   
+    srand(time(0));
+    
+    do {
+        string palavraSecreta = escolherPalavra();
+        vector<char> letrasAdivinhadas;
+        int tentativasRestantes = 6;
+        bool ganhou = false;
+        string mensagem = "";
 
-    std::vector<char> guessed;
-    int chances = 6;
-    char guess;
+        while (tentativasRestantes > 0 && !ganhou) {
+            limparTela();
+            cout << HANGMAN[6 - tentativasRestantes] << endl;
+            cout << "Tentativas restantes: " << tentativasRestantes << endl;
+            mostrarProgresso(palavraSecreta, letrasAdivinhadas);
 
-    while (chances > 0) {
-        clearScreen();
-        printHangman(chances);
-        printWord(guessed, word);
-        std::cout << "Chances restantes: " << chances << std::endl;
-        std::cout << "Digite uma letra: ";
-        std::cin >> guess;
-
-        guess = std::tolower(guess); // Converter para minúscula
-
-        if (!std::isalpha(guess)) {
-            std::cout << "Entrada inválida. Por favor, digite uma letra." << std::endl;
-#ifdef _WIN32
-            Sleep(2000); // Pausar para que o usuário veja a mensagem (2 segundos)
-#else
-            sleep(2); // Pausar para que o usuário veja a mensagem (2 segundos)
-#endif
-            continue;
-        }
-
-        if (std::find(guessed.begin(), guessed.end(), guess) == guessed.end()) {
-            guessed.push_back(guess);
-            if (word.find(guess) == std::string::npos) {
-                --chances;
+            if (!mensagem.empty()) {
+                cout << mensagem << endl;
+                mensagem = "";
             }
-        }
 
-        bool won = true;
-        for (char c : word) {
-            if (std::find(guessed.begin(), guessed.end(), c) == guessed.end()) {
-                won = false;
-                break;
+            char palpite;
+            cout << "Digite uma letra: ";
+            cin >> palpite;
+            palpite = tolower(palpite);
+
+            if (!isalpha(palpite)) {
+                mensagem = "Por favor, digite apenas letras!";
+                continue;
             }
+
+            if (find(letrasAdivinhadas.begin(), letrasAdivinhadas.end(), palpite) != letrasAdivinhadas.end()) {
+                mensagem = "Voce ja tentou essa letra!";
+                continue;
+            }
+
+            letrasAdivinhadas.push_back(palpite);
+
+            if (palavraSecreta.find(palpite) == string::npos) {
+                tentativasRestantes--;
+                mensagem = "Letra errada!";
+            } else {
+                mensagem = "Boa! Acertou uma letra.";
+            }
+
+            ganhou = all_of(palavraSecreta.begin(), palavraSecreta.end(), [&](char c) {
+                return find(letrasAdivinhadas.begin(), letrasAdivinhadas.end(), c) != letrasAdivinhadas.end();
+            });
         }
 
-        if (won) {
-            clearScreen();
-            printHangman(chances);
-            std::cout << "Você ganhou! A palavra era: " << word << std::endl;
-            break;
+        limparTela();
+        cout << HANGMAN[6 - tentativasRestantes] << endl;
+        if (ganhou) {
+            cout << "Parabens! Voce venceu! A palavra era: " << palavraSecreta << endl;
+        } else {
+            cout << "Que pena! Voce perdeu. A palavra era: " << palavraSecreta << endl;
         }
-    }
 
-    if (chances == 0) {
-        clearScreen();
-        printHangman(chances);
-        std::cout << "Você perdeu! A palavra era: " << word << std::endl;
-    }
-    system("pause");
+    } while (jogarNovamente());
+
+    cout << "Obrigado por jogar! Ate a próxima!" << endl;
     return 0;
+}
 }
